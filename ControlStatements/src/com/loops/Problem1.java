b84372704ac795cad0bd046290476c58d3e522b6
atul <atul.vaibhav1993@gmail.com>
2023-12-28 13:04:39 +0530

    code-28/12

---
+++ ControlStatements/src/com/loops/Problem1.java
@@ -0,0 +1,19 @@
+package com.loops;
+
+public class Problem1 {
+    public static int sumOfDigit(int num){
+    	int sum =0;
+    	while(num!=0){
+    		int rem = num%10;
+    		num = num/10;
+    		sum = sum+rem;
+    	}
+    	return sum;
+    }
+	public static void main(String[] args) {
+		int sum = Problem1.sumOfDigit(6734);
+		System.out.println(sum);
+
+	}
+
+}
