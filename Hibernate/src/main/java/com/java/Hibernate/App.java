7ef3a16e3f8bc182e2b126a7d9c078d0b031c5f4
atul <atul.vaibhav1993@gmail.com>
2024-02-12 15:59:42 +0530

    code-12/02

---
+++ Hibernate/src/main/java/com/java/Hibernate/App.java
@@ -0,0 +1,29 @@
+package com.java.Hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.java.Hibernate.entity.Student;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	Student s1 = new Student();
+    	s1.setName("Aayush");
+    	s1.setCourse("Java");
+    	
+        Configuration con = new Configuration().configure("abc.xml");
+        SessionFactory factory = con.buildSessionFactory();
+        Session session = factory.openSession();
+        session.beginTransaction();
+        session.save(s1);
+        session.getTransaction().commit();
+        session.close();
+        factory.close();
+        
+    }
+}
